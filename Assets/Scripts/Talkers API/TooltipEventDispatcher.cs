8d0278b9d80fecea58ae32a88b984fef406663b6
Keyboard Destroyer <grudko2003@gmail.com>
2025-06-18 16:35:40 +0300

    Abstracted base class of tooltip event dispatcher

--- Assets/Scripts/Talkers API/TooltipEventDispatcher.cs
+++ Assets/Scripts/Talkers API/TooltipEventDispatcher.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 
-public class TooltipEventDispatcher : MonoBehaviour
+public abstract class TooltipEventDispatcher : MonoBehaviour
 {
     [Header("Event Dispatcher")]
     [SerializeField] protected TooltipBase tooltip;
