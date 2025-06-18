2fc03de4c0f0807bcb265907ea4373da074ad2c5
Keyboard Destroyer <grudko2003@gmail.com>
2025-06-18 17:52:50 +0300

    Added serialization algo and related unit tests

---
+++ Assets/Scripts/Filesystem/Serializer.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using UnityEngine;
+
+public class Serializer
+{
+    public static void WriteData(SerializedType serializable)
+    {
+        var path = Application.persistentDataPath + "/" + serializable.GetType().Name + ".json";
+
+        var serializer = new DataContractJsonSerializer(serializable.GetType());
+
+        using var stream = new FileStream(path, FileMode.OpenOrCreate);
+        serializer.WriteObject(stream, serializable);
+    }
+
+    public static SerializedType ReadData<T>()
+    {
+        if (typeof(T).BaseType != typeof(SerializedType))
+            throw new NotImplementedException();
+        
+        var path = Application.persistentDataPath + "/" + typeof(T).Name + ".json";
+
+        if (!File.Exists(path))
+            throw new FileNotFoundException();
+        
+        var serializer = new DataContractJsonSerializer(typeof(T));
+        
+        using var stream = new FileStream(path, FileMode.Open);
+        return serializer.ReadObject(stream) as SerializedType;
+    }
+}
